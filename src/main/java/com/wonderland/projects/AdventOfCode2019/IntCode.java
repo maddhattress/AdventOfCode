241356eb7791d6d6260513c32285b716441e0782
maddhattress <maddhattress@gmail.com>
2020-09-27 15:18:23 -0400

    enhanced intCode to take in varying file inputs based on filename

--- src/main/java/com/wonderland/projects/AdventOfCode2019/IntCode.java
+++ src/main/java/com/wonderland/projects/AdventOfCode2019/IntCode.java
@@ -1,54 +1,58 @@
 package com.wonderland.projects.AdventOfCode2019;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 
 public class IntCode {
-
-	private static final int[] BASE = { 3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1002, 114, 19, 224, 1001, 224,
-			-646, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 7, 224, 1, 223, 224, 223, 1101, 40, 62, 225, 1101, 60, 38,
-			225, 1101, 30, 29, 225, 2, 195, 148, 224, 1001, 224, -40, 224, 4, 224, 1002, 223, 8, 223, 101, 2, 224, 224,
-			1, 224, 223, 223, 1001, 143, 40, 224, 101, -125, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 3, 224, 1,
-			224, 223, 223, 101, 29, 139, 224, 1001, 224, -99, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 2, 224, 1, 224,
-			223, 223, 1101, 14, 34, 225, 102, 57, 39, 224, 101, -3420, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 7,
-			224, 1, 223, 224, 223, 1101, 70, 40, 225, 1102, 85, 69, 225, 1102, 94, 5, 225, 1, 36, 43, 224, 101, -92,
-			224, 224, 4, 224, 1002, 223, 8, 223, 101, 1, 224, 224, 1, 224, 223, 223, 1102, 94, 24, 224, 1001, 224,
-			-2256, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 1, 224, 1, 223, 224, 223, 1102, 8, 13, 225, 1101, 36, 65,
-			224, 1001, 224, -101, 224, 4, 224, 102, 8, 223, 223, 101, 3, 224, 224, 1, 223, 224, 223, 4, 223, 99, 0, 0,
-			0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999,
-			1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227,
-			274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1,
-			99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 8, 677,
-			226, 224, 1002, 223, 2, 223, 1006, 224, 329, 1001, 223, 1, 223, 1108, 226, 226, 224, 1002, 223, 2, 223,
-			1005, 224, 344, 101, 1, 223, 223, 1108, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 359, 101, 1, 223, 223,
-			107, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 374, 101, 1, 223, 223, 1107, 226, 226, 224, 1002, 223, 2,
-			223, 1005, 224, 389, 101, 1, 223, 223, 107, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 404, 101, 1, 223,
-			223, 1008, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 419, 101, 1, 223, 223, 108, 677, 226, 224, 1002,
-			223, 2, 223, 1006, 224, 434, 101, 1, 223, 223, 1108, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 449, 101,
-			1, 223, 223, 1008, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 464, 1001, 223, 1, 223, 108, 677, 677, 224,
-			102, 2, 223, 223, 1005, 224, 479, 101, 1, 223, 223, 7, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 494,
-			1001, 223, 1, 223, 8, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 509, 101, 1, 223, 223, 107, 677, 226, 224,
-			1002, 223, 2, 223, 1005, 224, 524, 1001, 223, 1, 223, 7, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 539,
-			1001, 223, 1, 223, 1007, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 554, 1001, 223, 1, 223, 8, 677, 677,
-			224, 102, 2, 223, 223, 1006, 224, 569, 101, 1, 223, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 584,
-			1001, 223, 1, 223, 1008, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 599, 101, 1, 223, 223, 1007, 677, 677,
-			224, 1002, 223, 2, 223, 1006, 224, 614, 101, 1, 223, 223, 1107, 677, 226, 224, 1002, 223, 2, 223, 1006, 224,
-			629, 101, 1, 223, 223, 1107, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 644, 101, 1, 223, 223, 1007, 226,
-			226, 224, 102, 2, 223, 223, 1005, 224, 659, 1001, 223, 1, 223, 108, 226, 226, 224, 102, 2, 223, 223, 1006,
-			224, 674, 101, 1, 223, 223, 4, 223, 99, 226 };
-
-	private int[] code = BASE.clone();
 	
+	/** used to store program instruction/code**/
+	private int[] code;
+	BufferedReader reader = null;
+	
+	
+	/**
+	 * hardcode to day5 input if none provided
+	 */
+	public IntCode() {
+		this("input/day5.txt");
+
+	}
+	
+	/**
+	 * load up the program instructions/code based on input filename parameter
+	 * @param filename
+	 */
+	public IntCode(String filename) {
+		try {
+			reader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(filename)));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				code = ArrayUtils.addAll(code, Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray());
+			}
+		} catch (IOException ex) {
+			System.err.println("ERROR reading file: " + ex.getMessage());
+		} finally {
+			IOUtils.closeQuietly(reader);
+		}
+
+	}
+
 	/**
 	 * main processing unit
 	 * @param userInput
+	 * @returns the output for a given instruction
 	 */
-	private void run(int userInput) {
+	private int run(int userInput) {
 		int index = 0;
 		boolean calculating = true;
+		int retVal = 0;
 
 		while (calculating) {
 			int opCode = code[index];
@@ -96,7 +100,7 @@ public class IntCode {
 				index += 2;
 				break;
 			case 4:
-				System.out.println("Output: " + determineParam(code[index + 1], paramModeArray[0]));
+				retVal = determineParam(code[index + 1], paramModeArray[0]);
 				index += 2;
 				break;
 			case 5:
@@ -134,6 +138,7 @@ public class IntCode {
 			}
 
 		}
+		return retVal;
 
 	}
 	
@@ -170,7 +175,8 @@ public class IntCode {
 	public static void main(String[] args) {
 		IntCode ic = new IntCode();
 		int userInput = Integer.parseInt(IntCode.getUserInput());
-		ic.run(userInput);
+		int output = ic.run(userInput);
+		System.out.println("Output: " + output );
 
 	}
 
