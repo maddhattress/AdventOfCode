6de70080755b27cd2f9a8b65762475f9315eda1e
maddhattress <maddhattress@gmail.com>
2020-10-12 14:35:39 -0400

    Cleaned up 2015 Day 9 Code

--- src/main/java/com/wonderland/projects/AdventOfCode2015/RouteCalculator.java
+++ src/main/java/com/wonderland/projects/AdventOfCode2015/RouteCalculator.java
@@ -16,6 +16,12 @@ import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * @author Jenn Dell
+ * @see <a
+ *      href="Advent of Code 2015, Day 9">https://adventofcode.com/2015/day/9</a>
+ *
+ */
 public class RouteCalculator {
 	private static final Logger log = LogManager.getLogger();
 	private static final String INPUT = "input/2015/day9.txt";
@@ -34,7 +40,7 @@ public class RouteCalculator {
 	 */
 	public void run() {
 		List<String> routes = this.readRoutes();
-		for(String route : routes) {
+		for (String route : routes) {
 			log.debug("Parsing Route[" + route + "].");
 			Matcher m = ROUTE_PATTERN.matcher(route);
 			// check if instruction is valid.
@@ -42,40 +48,54 @@ public class RouteCalculator {
 				String origin = m.group(1);
 				String destination = m.group(2);
 				int distance = Integer.parseInt(m.group(3));
-				//add both to and from to map
+				// add both to and from to map
 				this.addToMap(origin, destination, distance);
 				this.addToMap(destination, origin, distance);
-			}else {
+			} else {
 				log.error("Unknown format for Route[" + route + "].");
 			}
 		}
-		//print map for debugging
+		// print map for debugging
 		this.printRouteMap();
-		//calculate all possibilities
+		// calculate all possibilities
 		Map<List<String>, Integer> calculatedRoutes = this.calculateAllRoutes();
-		log.info("Shortest Route: " +  Collections.min(calculatedRoutes.values()));
-		log.info("Longest Route: " +  Collections.max(calculatedRoutes.values()));
+		log.info("Shortest Route: " + Collections.min(calculatedRoutes.values()));
+		log.info("Longest Route: " + Collections.max(calculatedRoutes.values()));
 
 	}
 
+	/**
+	 * helper function to 2 points and the distance between them to the map
+	 * 
+	 * @param origin
+	 * @param destination
+	 * @param distance
+	 */
 	private void addToMap(String origin, String destination, int distance) {
-		if(!routeMap.containsKey(origin)) {
-			routeMap.put(origin, new HashMap<String, Integer>());    
+		if (!routeMap.containsKey(origin)) {
+			routeMap.put(origin, new HashMap<String, Integer>());
 		}
 		routeMap.get(origin).put(destination, distance);
 	}
-	
+
+	/**
+	 * calculates all distance of all possible routes
+	 * 
+	 * @return
+	 */
 	private Map<List<String>, Integer> calculateAllRoutes() {
 		log.info("Calculating Routes:");
-		//hacking shortest route because 'each location must be visited exactly once'
+		// hacking shortest route because 'each location must be visited exactly once'
 		PermutationIterator<String> iter = new PermutationIterator<String>(routeMap.keySet());
 		Map<List<String>, Integer> calculatedRoutes = new HashMap<List<String>, Integer>();
 		while (iter.hasNext()) {
 			List<String> route = iter.next();
-			int distance = 0; 
+			int distance = 0;
 			String origin = null;
-			for(String destination : route) {
-				if(origin != null) {
+			// go thru the route and figure out the distance between each point to
+			// identify total distance for entire route
+			for (String destination : route) {
+				if (origin != null) {
 					int distanceBetween = routeMap.get(origin).get(destination);
 					log.debug("Distance between [" + origin + "] --> [" + destination + "] = " + distanceBetween);
 					distance += distanceBetween;
@@ -83,9 +103,8 @@ public class RouteCalculator {
 				}
 				origin = destination;
 			}
-			//if not in route list. add
-			//TODO: fix bug with anymatch
-			if(!calculatedRoutes.keySet().contains(route)) {
+			// if not in route list. add
+			if (!calculatedRoutes.keySet().contains(route)) {
 				log.debug("Adding Route[" + route.toString() + "] with distance [" + distance + "] to map.");
 				calculatedRoutes.put(route, distance);
 			}
@@ -93,14 +112,17 @@ public class RouteCalculator {
 		log.debug("Total routes: " + calculatedRoutes.size());
 		return calculatedRoutes;
 	}
-	
-	@SuppressWarnings("unused")
+
+	/**
+	 * helper function to print out the map neatly
+	 */
 	private void printRouteMap() {
 		log.info("Printing Route Map:");
-		for(Entry<String, Map<String, Integer>> sourceRoutes : routeMap.entrySet()) {
-		 log.info(sourceRoutes.toString());	
-		 }
+		for (Entry<String, Map<String, Integer>> sourceRoutes : routeMap.entrySet()) {
+			log.info(sourceRoutes.toString());
+		}
 	}
+
 	/**
 	 * reads the instructions from the input file
 	 * 
